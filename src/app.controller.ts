09cb9f150d5f8c9a1ea6ef1537124a68aed679ba
Chingiz Kheychiev <heichiev@mail.ru>
2023-05-22 15:27:47 +0300

    create chat

---
+++ src/app.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/chat')
+  @Render('index')
+  Home(): string {
+    return;
+  }
+
+  @Get('/api/chat')
+  async getMessages(@Res() res) {
+    const messages = await this.appService.getMessages();
+    res.json(messages);
+  }
+}
+
+// import { Controller, Get, Render, Res } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get('/chat')
+//   @Render('index')
+//   Home(): string {
+//     return;
+//   }
+
+// }
