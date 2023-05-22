135c8d9c63cb358accb291ff36921d10aebd7935
ChingizKheychiev <113612167+ChingizKheychiev@users.noreply.github.com>
2023-05-22 15:36:55 +0300

    edit URL

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 const app = () => {
-  const socket = io('http://localhost:3000');
+  const socket = io('https://chat08.onrender.com');
   const msgInput = document.querySelector('.message-input');
   const msgList = document.querySelector('.messages-list');
   const sendBtn = document.querySelector('.send-btn');
@@ -7,7 +7,7 @@ const app = () => {
   const messages = [];
   const getMessages = async () => {
     try {
-      const { data } = await axios.get('http://localhost:3000/api/chat');
+      const { data } = await axios.get('https://chat08.onrender.com/api/chat');
 
       renderMessages(data);
 
